b21fe6024c6d0b19b4ec773991b02666f3b9928e
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-08-09 09:59:23 -0300

    Created simple Character class to keep track of hp

---
+++ HandRehab/Assets/Scripts/Character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int maxHp;
+    int hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Fallback hp
+        if (maxHp == 0) {
+            maxHp = 100;
+        }
+        hp = maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp == 0) {
+            if (this.tag != "Player") {
+                GameObject.DestroyImmediate(this);
+            }
+        }
+    }
+
+    void Hit(int damage) {
+        hp -= damage;
+        if (hp < 0) {
+            hp = 0;
+        }
+    }
+}
