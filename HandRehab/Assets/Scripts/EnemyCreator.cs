3af4bd0ea84f8a2b1116768b39c015bf5b64a88d
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-08-22 10:05:01 -0300

    Fixed enemy y on creation

--- HandRehab/Assets/Scripts/EnemyCreator.cs
+++ HandRehab/Assets/Scripts/EnemyCreator.cs
@@ -7,14 +7,18 @@ public class EnemyCreator : MonoBehaviour
     public GameObject enemy;
     public GameObject player;
     public int numberOfEnemies;
+    public Terrain terrain;
 
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < numberOfEnemies; i++) {
             GameObject copy = GameObject.Instantiate(enemy);
-            copy.transform.position = player.transform.position + player.transform.forward * 20 + Vector3.up * 3;
+            copy.transform.position = player.transform.position + player.transform.forward * 20;
             copy.transform.RotateAround(player.transform.position, Vector3.up, 360 / numberOfEnemies * i);
+            Vector3 enemyPosition = copy.transform.position;
+            enemyPosition.y = terrain.SampleHeight(enemyPosition) + terrain.transform.position.y + 1;
+            copy.transform.position = enemyPosition;
         }
     }
 
