3a7ae0e09cfbf3b584bcacfca70a560364195243
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-10-10 11:01:54 -0300

    Added music to the game

---
+++ HandRehab/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public List<AudioClip> songs;
+    public AudioSource audioSource;
+    int currentSong = 0;
+    // Start is called before the first frame update
+    void Start() {
+        songs = Randomize(songs).ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(songs.Count > 0 && !audioSource.isPlaying) {
+            currentSong = (currentSong + 1) % songs.Count;
+            audioSource.clip = songs[currentSong];
+            audioSource.Play();
+        }
+    }
+
+    public static IEnumerable<T> Randomize<T>(IEnumerable<T> source) {
+        return source.OrderBy<T, int>((item) => Random.Range(0, source.Count() - 1));
+    }
+}
